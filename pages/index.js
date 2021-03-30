38362734b86fb39b9199a76021206b8415b9a339
Darion Higgins <dhiggins@shelterinsurance.com>
2021-03-30 15:02:39 -0500

    init

--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,41 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Arvo"/>
+                <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Montserrat"/>
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main className={styles.main}>
+                <div className={styles.split}>
+                    <div className={styles.column}>
+                        <h1>
+                            <strong>Hi! I’m a full-stack developer based in Columbia, MO.</strong>
+                        </h1>
+                        <p>
+                            Focus in Shopify, WordPress, React/Angular web-apps, and cross-platform mobile applications. With almost a decade of experience I can bring your project to life.
+                        </p>
+                    </div>
+                    <div className={styles.column}>
+                        <img width={"100%"} src={"https://www.centerforempathy.org/wp-content/uploads/2019/11/placeholder.png"}/>
+                    </div>
+                </div>
+            </main>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
